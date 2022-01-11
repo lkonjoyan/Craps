98b50cef9ba4fc717adc7fa6657b2eb1ebcee4cc
Libby Konjoyan <lkonjoyan@outlook.com>
2022-01-11 09:26:55 -0800

    Wrote the Die class + beginnings of the Craps class

---
+++ Craps.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class Craps here.
+ *
+ * @author Libby Konjoyan
+ * @version 11.1.22
+ */
+
+import java.util.Scanner;
+
+public class Craps
+{
+    //instructions method
+    public static void main(String[] args)
+    {
+        //welcome
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello, would you like to play a game of craps? (yes or no) ");
+        String response = in.nextLine();
+        if (response != "yes")
+        {
+            //end program
+        }
+        //call instructions method
+        //dice rolling --> call die class
+        
+    }
+}
---
+++ Die.java
@@ -0,0 +1,42 @@
+
+/**
+ * Write a description of class Die here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Die
+{
+    // instance variables - replace the example below with your own
+    private int roll;
+
+    /**
+     * Constructor for objects of class Die
+     * Give the die a random initial value
+     */
+    public Die()
+    {
+        // initialise instance variables
+        roll = rollDie();
+    }
+
+    /**
+     * Rolls the die
+     * @return the value of the die after the roll;
+     */
+    public int rollDie()
+    {
+        // put your code here
+        roll = (int)(Math.random() * 6 +1);
+        return roll;
+    }
+    
+    /**
+     * Gets the value of the die without rolling it;
+     * @return the value of the die;
+     */
+    public int getRoll()
+    {
+        return roll;
+    }
+}
